bc4fa888089b1f056bc9a06c447e5d48784fbd76
squidserv <mprovost1992@gmail.com>
2021-10-04 18:27:14 -0400

    Add win condition, add tutorial

--- CultistRole/lua/customroles/cultist.lua
+++ CultistRole/lua/customroles/cultist.lua
@@ -79,6 +79,17 @@ table.insert(ROLE.convars, {
 print("Registered role")
 RegisterRole(ROLE)
 
+hook.Add("Initialize", "CultistInitialize", function()
+    -- Use 323 if we're on Custom Roles for TTT earlier than version 1.2.5
+    -- 323 is summation of the ASCII values for the characters "C", "L", and "T"
+    WIN_CULTIST = GenerateNewWinID and GenerateNewWinID() or 323
+
+    if CLIENT then
+        LANG.AddToLanguage("english", "win_cultist", "The {role} and their minions have overwhelmed their enemies!")
+        LANG.AddToLanguage("english", "ev_win_cultist", "The {role} and their army of minions has won them the round!")
+    end
+end)
+
 if SERVER then
     AddCSLuaFile()
 
@@ -99,6 +110,63 @@ if SERVER then
                     p:PrintMessage(HUD_PRINTCENTER, "There is ".. ROLE_STRINGS_EXT[ROLE_CULTIST])
                 end
             end
+        end
+    end)
+
+    hook.Add("TTTCheckForWin", "CultistCheckForWin", function()
+        local cultistAlive = false
+        local otherAlive = false
+        for _, v in ipairs(player.GetAll()) do
+            if v:Alive() and v:IsTerror() then
+                if v:IsCultist() then
+                    cultistAlive = true
+                elseif not v:ShouldActLikeJester() then
+                    otherAlive = true
+                end
+            end
+        end
+
+        if cultistAlive then
+            if not otherAlive then
+                return WIN_CULTIST
+            else
+                return WIN_NONE
+            end
+        end
+    end)
+
+    hook.Add("TTTPrintResultMessage", "CultistPrintResultMessage", function(type)
+        if type == WIN_CULTIST then
+            LANG.Msg("win_cultist", { role = ROLE_STRINGS[ROLE_CULTIST] })
+            ServerLog("Result: " .. ROLE_STRINGS[ROLE_CULTIST] .. " wins.\n")
+            return true
+        end
+    end)
+end
+
+if CLIENT then
+    hook.Add("TTTEventFinishText", "CultistEventFinishText", function(e)
+        if e.win == WIN_CULTIST then
+            return LANG.GetParamTranslation("ev_win_cultist", { role = ROLE_STRINGS[ROLE_CULTIST]:lower() })
+        end
+    end)
+
+    hook.Add("TTTEventFinishIconText", "CultistEventFinishIconText", function(e, win_string, role_string)
+        if e.win == WIN_CULTIST then
+            return win_string, ROLE_STRINGS_PLURAL[ROLE_CULTIST]
+        end
+    end)
+
+    hook.Add("TTTScoringWinTitle", "CultistScoringWinTitle", function(wintype, wintitles, title, secondaryWinRole)
+        if wintype == WIN_CULTIST then
+            return { txt = "hilite_win_role_singular", params = { role = ROLE_STRINGS[ROLE_CULTIST]:upper() }, c = ROLE_COLORS[ROLE_CULTIST] }
+        end
+    end)
+
+    hook.Add("TTTTutorialRoleText", "CultistTutorialRoleText", function(role, titleLabel, roleIcon)
+        if role == ROLE_CULTIST then
+            local roleColor = ROLE_COLORS[ROLE_CULTIST]
+            return "The " .. ROLE_STRINGS[ROLE_CULTIST] .. " is an <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>independent role</span> who must convince fellow players to join their team."
         end
     end)
 end
